d37d337b6ce618c219102335e9659841570b2ab2
Cameron Lay <cameronlay15@gmail.com>
2020-11-03 17:42:00 -0500

    app and index

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Navigation from './pages/Navigation'
+import About from './pages/About'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
+import Footer from './pages/Footer'
+
+export default function App() {
+  return (
+    <div>
+    <Navigation />
+    
+  <Switch>
+    <Route exact path="/" component={About} />
+    <Route exact path="/portfolio" component={Portfolio} />
+    <Route exact path="/contact" component={Contact} />
+  </Switch>
+
+    <Footer />
+    </div>
+  )
+}
