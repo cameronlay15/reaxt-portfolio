3e78a6afb7cdeb3ef286dcbc55264d30c4687542
Cameron Lay <cameronlay15@gmail.com>
2020-11-03 17:37:20 -0500

    footer

---
+++ src/pages/footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar'
+import './footer.css';
+
+export default function Footer() {
+  return (
+
+<div>
+<Navbar expand="lg" bg="dark" fixed="bottom">
+
+      <div class="text-light icon">
+
+        <a href="https://github.com/cameronlay15" target="new-gh" style={{ marginRight: '.25rem' }}><i class="fab fa-github-square"></i></a>
+        <a href="https://linkedin.com/cameronlay15" target="new-li" style={{ marginLeft: '.25rem' }}><i class="fab fa-linkedin"></i></a>
+
+      </div>
+
+ </Navbar>
+</div>
+
+  )
+}
