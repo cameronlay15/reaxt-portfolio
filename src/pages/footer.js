8d68e8fe306241f55d512603ace862205251bfda
Cameron Lay <cameronlay15@gmail.com>
2020-11-04 09:58:00 -0500

    updated footer again

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
       <div class="text-light icon">
 
         <a href="https://github.com/cameronlay15" target="new-gh" style={{ marginRight: '.25rem' }}><i class="fab fa-github-square"></i></a>
-        <a href="https://linkedin.com/in/cameronlay15" target="new-li" style={{ marginLeft: '.25rem' }}><i class="fab fa-linkedin"></i></a>
+        <a href="https://linkedin.com/in/cameronlay" target="new-li" style={{ marginLeft: '.25rem' }}><i class="fab fa-linkedin"></i></a>
 
       </div>
 
